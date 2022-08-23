dda064b20c1574a6321c687ac6a3d1c87ad59e65
demeritbird <lekjiewei@hotmail.sg>
2022-08-23 12:03:21 +0800

    feat: add default recyclerView & bottomNavMenu

--- app/src/main/java/com/example/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.sampleproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.sampleproject.fragments.HomeFragment;
+import com.example.sampleproject.fragments.NotificationFragment;
+import com.example.sampleproject.fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,7 +36,11 @@ public class MainActivity extends AppCompatActivity {
         // TODO: find out how to traverse different collection and keyvalue pairs.
         FirebaseDatabase database = FirebaseDatabase.getInstance(getResources().getString(R.string.firebase_link));
         DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        myRef.setValue("THIS asdasdasdasd WORK LA too, World!");
+
+        DatabaseReference database1 = FirebaseDatabase.getInstance(getResources().getString(R.string.firebase_link)).getReference();
+        DatabaseReference myRef1 = database1.child("events");
+        myRef1.child("person1").child("firstname").setValue("james");
 
 
         // Read from the database
@@ -59,6 +70,43 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Log.w(TAG, "Good Morning!");
                 testView.setText("Good Morning!");
+            }
+
+
+        });
+
+        Button toSubAct = findViewById(R.id.to_sub_act);
+        toSubAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSub = new Intent(MainActivity.this, SubActivity.class);
+                intentSub.putExtra("message", testView.getText());
+                startActivity(intentSub);
+            }
+        });
+
+        //// Fragment ////
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        HomeFragment homeFragment = new HomeFragment();
+        NotificationFragment notificationFragment = new NotificationFragment();
+        SettingsFragment settingsFragment = new SettingsFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, notificationFragment).commit();
+                        return true;
+                    case R.id.settings:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingsFragment).commit();
+                        return true;
+                }
+                return false;
             }
         });
     }
---
+++ app/src/main/java/com/example/sampleproject/SubActivity.java
@@ -0,0 +1,99 @@
+package com.example.sampleproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SubActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    personAdaptar adapter; // Create Object of the Adapter class
+    DatabaseReference mbase; // Create object of the Firebase Realtime Database
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        TextView textView = findViewById(R.id.sub_testview);
+
+
+        Bundle resultIntent = getIntent().getExtras();
+        String intentValue = resultIntent.getString("message");
+
+
+        textView.setText(intentValue);
+
+        ///// Recycler View ////
+
+        // Create a instance of the database and get
+        // its reference
+        mbase = FirebaseDatabase.getInstance(getResources().getString(R.string.firebase_link)).getReference().child("events");
+
+        recyclerView = findViewById(R.id.recycler1);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<person> options
+                = new FirebaseRecyclerOptions.Builder<person>()
+                .setQuery(mbase, person.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new personAdaptar(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+
+        /////
+
+        Button addMember = findViewById(R.id.add_member);
+        addMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference database1 = FirebaseDatabase.getInstance(getResources().getString(R.string.firebase_link)).getReference().child("events");
+                person newguy = new person("firstjhin", "lastjhin", "24");
+                database1.child("person4").child("firstname").setValue(newguy.getFirstname());
+                database1.child("person4").child("lastname").setValue(newguy.getFirstname());
+                database1.child("person4").child("age").setValue(newguy.getAge());
+            }
+        });
+
+
+
+    }
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override protected void onStart()
+    {
+        super.onStart();
+        ///// Recycler View ////
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        ///// Recycler View ////
+        adapter.stopListening();
+    }
+
+
+
+
+
+}
